e5f39a37c483e39f75d0090fc1bef9f96c0ad424
ceeriil <simonorugbo15@gmail.com>
2025-02-13 04:54:22 +0100

    init tailwind

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,15 +2,16 @@ import { ConnectButton } from "@/components/ConnectButton";
 import { InfoList } from "@/components/InfoList";
 import { ActionButtonList } from "@/components/ActionButtonList";
 export default function Home() {
-
   return (
     <div className={"pages"}>
-      <h1>AppKit Solana Next.js App Router Example</h1>
+      <h1 className="text-4xl text-red-200">
+        AppKit Solana Next.js App Router Example
+      </h1>
 
-      <div >
+      <div>
         <ConnectButton />
       </div>
-      <div >
+      <div>
         <ActionButtonList />
       </div>
       <InfoList />
